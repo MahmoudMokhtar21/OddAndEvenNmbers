a8939041cbb0d9b29078ef53f8c068f5e9eb7b5f
Mahmoud-Mokhtar <mahmoudmoukhtar93@gmail.com>
2025-10-17 15:33:32 +0300

    initial commit

---
+++ OddAndEvenNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System.Linq;
+
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+PrintNumbers("numbers", numbers);
+PrintNumbers("Even Numbers", numbers.Where(x => IsEven(x)));
+PrintNumbers("Odd Numbers", numbers.Where(x => IsOdd(x)));
+
+
+
+static void PrintNumbers(string title,IEnumerable<int> nums)
+{
+    Console.WriteLine();
+    Console.Write($"{title} : [");
+    foreach(int n in nums)
+    {
+        Console.Write(n);
+    }
+    Console.Write(" ]");
+}
+
+static bool IsEven(int n)
+{
+    if (n % 2 == 0)
+        return true;
+    else
+        return false;
+}
+static bool IsOdd(int n)
+{
+    if (n % 2 == 0)
+        return false;
+    else
+        return true;
+
+}
